5ed1ab62486940b39cd7d5a55635552963ed8fb7
stanley <stanleyhuang83@gmail.com>
2017-10-18 20:18:30 -0700

    migration fix #2

--- db/migrate/20170415135855_create_excersizes.rb
+++ db/migrate/20170415135855_create_excersizes.rb
@@ -1,6 +1,6 @@
-class Createexercises < ActiveRecord::Migration[5.0]
+class CreateExcersizes < ActiveRecord::Migration[5.0]
   def change
-    create_table :exercises do |t|
+    create_table :excersizes do |t|
       t.text :question
       t.text :answer
       t.references :section, foreign_key: true
